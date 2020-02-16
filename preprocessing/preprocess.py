707a4d7b945c0cd3f475e603cb825d2fe9100675
Kimberly <kmilam@google.com>
2020-02-16 18:41:43 -0500

    Add additional methods and tests

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -1,5 +1,6 @@
 """Preprocessing library to clean and embed tweets"""
 
+import json
 from urllib.parse import urlparse
 
 from nltk.tokenize import TweetTokenizer
@@ -7,22 +8,24 @@ import tensorflow as tf
 
 class PreprocessTweets():
     """Library to perform preprocessing for sentiment analysis on tweets."""
-    def __init__(self, text, max_length_dictionary=None, max_length_tweet=None):
-        self.text = text
+    def __init__(self, input_data, max_length_dictionary=None,
+                 max_length_tweet=None, token_indices_json=None):
+        self.input = input_data
         self.max_length_dictionary = max_length_dictionary
         self.max_length_tweet = max_length_tweet
+        self.token_indices_json = token_indices_json
 
 
     def clean_text(self):
         """Cleans text by removing URLs, hashtags, and usernames."""
         cleaned_tweet = ""
-        for word in self.text.split(" "):
+        for word in self.input.split(" "):
             if word.startswith("@") or word.startswith("#"):
                 continue
             result = urlparse(word)
             if all([result.scheme, result.netloc]):
                 continue
-            cleaned_tweet += word + " "
+            cleaned_tweet += word.lower() + " "
         return cleaned_tweet.strip()
 
 
@@ -36,4 +39,25 @@ class PreprocessTweets():
             An array of ints (tokens).
         """
         tknzr = TweetTokenizer()
-        return tknzr.tokenize(self.text)
+        return tknzr.tokenize(self.input)
+
+
+    def replace_token_with_index(self):
+        """Replaces each token with its index in the embedding dictionary."""
+        tokens = []
+        with tf.io.gfile.GFile(self.token_indices_json) as file:
+            indices = json.load(file)
+
+        for word in self.input:
+            index = indices[word]
+            max_length = self.max_length_dictionary
+            if not max_length or index < max_length:
+                tokens.append(index)
+        return tokens
+
+
+    def pad_sequence(self):
+        """Pads and slices list of indices."""
+        tokens = self.input[:self.max_length_tweet]
+        tokens = tokens + [0]*(self.max_length_tweet-len(tokens))
+        return tokens
--- preprocessing/test_preprocess.py
+++ preprocessing/test_preprocess.py
@@ -9,7 +9,7 @@ def clean_text(text):
 
 def test_clean_text():
     """Tests clean_text function."""
-    input_text = "@my_handler here is my tweet http://www.google.com got it?"
+    input_text = "@my_handler Here is my tweet http://www.google.com got it?"
     output_text = "here is my tweet got it?"
     assert clean_text(input_text) == output_text
 
@@ -24,3 +24,37 @@ def test_tokenize_text():
     input_text = "here is my tweet got it?"
     output_text = ["here", "is", "my", "tweet", "got", "it", "?"]
     assert tokenize_text(input_text) == output_text
+
+
+def replace_token_with_index(tokens):
+    """Calls replace_token_with_index method for testing."""
+    input_json = "preprocessing/token_indices.json"
+    return preprocess.PreprocessTweets(
+        tokens, token_indices_json=input_json).replace_token_with_index()
+
+
+def test_replace_token_with_index():
+    """Tests replace_token_with_index function."""
+    input_tokens = ["here", "is", "my", "tweet", "got", "it", "?"]
+    output_indices = [229, 32, 29, 274, 143, 33, 14]
+    assert replace_token_with_index(input_tokens) == output_indices
+
+
+def pad_sequence(indices, size):
+    """Calls pad_sequence method for testing."""
+    return preprocess.PreprocessTweets(
+        indices, max_length_tweet=size).pad_sequence()
+
+
+def test_pad_sequence_add_padding():
+    """Tests pad_sequence function for too short input."""
+    input_indices = [229, 32, 29, 274, 143, 33, 14]
+    output_indices = [229, 32, 29, 274, 143, 33, 14, 0, 0, 0]
+    assert pad_sequence(input_indices, 10) == output_indices
+
+
+def test_pad_sequence_slice():
+    """Tests pad_sequence function for too long input."""
+    input_indices = [229, 32, 29, 274, 143, 33, 14]
+    output_indices = [229, 32, 29, 274, 143]
+    assert pad_sequence(input_indices, 5) == output_indices
