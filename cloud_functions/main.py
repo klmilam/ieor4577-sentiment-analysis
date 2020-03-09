a17051bc1753cc75a63f6d35fb9bc6e82659d89e
Kimberly <kmilam@google.com>
2020-03-09 19:11:54 -0400

    update directory

--- cloud_functions/main.py
+++ cloud_functions/main.py
@@ -46,4 +46,4 @@ def predict(request):
     payload_log["request_time"] = str(request_time)
     payload_log["preprocess_time"] = str(preprocess_time - request_time)
     payload_log["inference_time"] = str(prediction_time - preprocess_time)
-    return response
+    return str(response)
